4588deaef99ea001703051fa897db2e6b2cad6bc
Ryan Lee <ryantlee9@gmail.com>
2017-03-28 18:14:26 -0400

    Splining

--- src/viz.py
+++ src/viz.py
@@ -5,12 +5,13 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import spline
 
 import config
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--naverage', type=int, help='number of periods over which to average', default=20)
+parser.add_argument('--naverage', type=int, help='number of periods over which to average', default=50)
 args = parser.parse_args()
 
 columns = [
@@ -25,18 +26,18 @@ with open(path.join(config.path_outputs, 'loss_history.pkl'), 'rb') as f:
     loss_history = pickle.load(f)
 
 # smooth loss curves via moving average
-losses = np.array(loss_history)
-smoothed = np.vstack(
-    [np.convolve(series, np.ones(args.naverage) / args.naverage)
-     for series in losses.T]).T[args.naverage - 1:-args.naverage + 1, :]
-
-# create x axis
 epochs = 25  # TODO: read this dynamically
-x = np.linspace(1, epochs, smoothed.shape[0])
+losses = np.array(loss_history)
+ma = np.vstack(
+    [np.convolve(series, np.ones(args.naverage) / args.naverage)
+     for series in losses.T]).T[args.naverage - 1:-args.naverage, :]
+x = np.linspace(1, epochs, ma.shape[0])
+xnew = np.linspace(x.min(), x.max(), epochs)
+smoothed = np.vstack([spline(x, s, xnew) for s in ma.T]).T
 
 fig, ax = plt.subplots()
 for i, series in enumerate(smoothed.T):
-    ax.plot(x, series, label=columns[i])
+    ax.plot(xnew, series, label=columns[i])
 
 box = ax.get_position()
 ax.set_position([box.x0, box.y0 + box.height * 0.2, box.width, box.height * 0.9])
